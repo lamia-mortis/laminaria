62c0db7015daa71ad280713e2cc72aa032b6344d
GitHub Action <action@github.com>
2023-08-04 18:27:06 +0000

    Template cleanup

--- src/main/kotlin/com/github/lamiamortis/laminaria/MyAction.kt
+++ src/main/kotlin/com/github/lamiamortis/laminaria/MyAction.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lamiamortis.laminaria
 
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
--- src/main/kotlin/com/github/lamiamortis/laminaria/MyBundle.kt
+++ src/main/kotlin/com/github/lamiamortis/laminaria/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.lamiamortis.laminaria
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
